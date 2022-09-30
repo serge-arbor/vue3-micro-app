9359d6cbb0d1853c9cf7c069511eae0c6c85eb98
Serge Arbor <serge.arbor@gmail.com>
2022-09-30 14:57:35 +0200

    microFE works!

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,47 @@
 import { createApp } from "vue";
+import type { App } from "vue";
 import { createPinia } from "pinia";
-import App from "./App.vue";
 
+import app from "./App.vue";
 import "./assets/main.css";
 
-const app = createApp(App);
-app.use(createPinia());
+import {
+  renderWithQiankun,
+  qiankunWindow,
+} from "vite-plugin-qiankun/dist/helper";
 
-app.mount("#micro-app-example");
+let root: App;
+
+function render(props: any) {
+  const { container } = props;
+  root = createApp(app);
+  root.use(createPinia());
+  const c = container
+    ? container.querySelector("#app")
+    : document.getElementById("app");
+  root.mount(c);
+}
+
+console.log('Loading micro-app');
+
+renderWithQiankun({
+  mount(props) {
+    console.log("micro-app mount");
+    render(props);
+  },
+  bootstrap() {
+    console.log("micro-app bootstrap");
+  },
+  unmount(props: any) {
+    console.log("micro-app unmount");
+    root.unmount();
+  },
+  update(props: any) {
+    console.log("micri-app update");
+    console.log(props);
+  },
+});
+
+if (!qiankunWindow.__POWERED_BY_QIANKUN__) {
+  render({});
+}
--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,16 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import qiankun from 'vite-plugin-qiankun';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), qiankun('MicroAppExample', { useDevMode: true })],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  base: 'http://127.0.0.1:3333/'
 });
